064766d256b071f361f71ee0b078c45aef581dfc
mandaapag03 <paganiamanda791@gmail.com>
2022-04-18 09:57:57 -0300

    Adiciona arquivo de soma

---
+++ desafios/lógicaSoma.py
@@ -0,0 +1,5 @@
+n1 = int(input('Informe 3 números para obter a média entre eles: \nn1 = '))
+n2 = int(input('n2 = '))
+n3 = int(input('n3 = '))
+media = (n1 + n2 + n3)/3
+print('A média entre {} , {} e {} vale {}'.format(n1, n2, n3, media))
