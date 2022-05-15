185f3c54ac15e988ce93a8b0c82ad59fdb66e2e6
Amanda Pagani <91763777+mandaapag03@users.noreply.github.com>
2022-05-15 14:45:27 -0300

    Adiciona solução para desafio020
    Ao colocar as variáveis em uma lista, é possível utilizar o método sample() para sortear elementos de forma ordenada e sem repetição, especificando apenas a quantidades de elementos sorteados.

--- desafios/desafio020.py
+++ desafios/desafio020.py
@@ -1,1 +1,5 @@
 #O professor quer determinar a ordem de apresentação de um trabalho, são quatro alunos.
+from random import sample
+n1, n2, n3, n4 = input('Informe os nomes dos alunos: ').split()
+nomes = [n1, n2, n3, n4]
+print(f'A ordem é {sample(nomes, 4)})')
