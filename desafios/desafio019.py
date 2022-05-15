291c2fd56ccbedb6b63f4c27f074ce1747fb3142
Amanda Pagani <91763777+mandaapag03@users.noreply.github.com>
2022-05-15 14:41:16 -0300

    Adiciona solução para o desafio019
    Ao colocar as variáveis inicializadas em uma lista, é possível utilizar o método choice() para sortear algum elemento.

--- desafios/desafio019.py
+++ desafios/desafio019.py
@@ -1,12 +1,17 @@
 #professor quer sortear 1 aluno dos 4
-import random
-a1, a2, a3, a4 = input('Informe o nome dos alunos: ').split()
-escolha = random.randint(1, 4)
-if escolha == 1:
-    print(a1)
-elif escolha == 2:
-    print(a2)
-elif escolha == 3:
-    print(a3)
-else:
-    print(a4)
+from random import choice
+n1, n2, n3, n4 = input('Informe o nome dos alunos: ').split()
+nomes = [n1, n2, n3, n4]
+print(choice(nomes))
+
+#import random
+#a1, a2, a3, a4 = input('Informe o nome dos alunos: ').split()
+#escolha = random.randint(1, 4)
+#if escolha == 1:
+#    print(a1)
+#elif escolha == 2:
+#    print(a2)
+#elif escolha == 3:
+#   print(a3)
+#else:
+#    print(a4)
