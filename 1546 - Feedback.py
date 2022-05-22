79ddc2a685e65163cf2ce0bb59311a6cef854fa1
Amanda Pagani <91763777+mandaapag03@users.noreply.github.com>
2022-05-22 19:59:58 -0300

    Create 1546 - Feedback.py

---
+++ 1546 - Feedback.py
@@ -0,0 +1,11 @@
+casos = int(input())
+i = 0
+while i < casos:
+    feedback = int(input())
+    j = 0
+    while j < feedback:
+        categ_feedback = int(input())
+        if categ_feedback == 1:
+            print()
+        j += 1
+    i += 1
