860334170dfffc13a9aab8760ac308a39dc5b016
Amanda Pagani <91763777+mandaapag03@users.noreply.github.com>
2022-05-22 17:58:48 -0300

    Create aula012p2.py

---
+++ anotacoes_aulas/aula012p2.py
@@ -0,0 +1,10 @@
+nome = input('Qual seu nome? ')
+if nome == 'Amanda':
+    print('Que nome lindo você tem :3')
+elif nome == 'Gine' or nome == 'Lauren' or nome == 'Pérola':
+    print('Seu nome é igual de uma das minhas gatinhas! <3')
+elif nome in 'Ana Claudia Jéssica Juliana':
+    print('Belo nome')
+else:
+    print('Seu nome é comum...')
+print(f'Tenha um bom dia, {nome}!!')
