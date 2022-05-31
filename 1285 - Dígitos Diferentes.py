60ee385212b64538df6da5fa3b187f3d7d12a9f1
Amanda Pagani <91763777+mandaapag03@users.noreply.github.com>
2022-05-31 11:21:02 -0300

    Create 1285 - Dígitos Diferentes.py

---
+++ 1285 - Dígitos Diferentes.py
@@ -0,0 +1,19 @@
+def separa(n):
+    l = []
+    for _ in range(4):
+        num = n % 10
+        l.append(num)
+        n = n // 10
+    l.reverse()
+    return l
+        
+x, y = input().split()
+x, y = int(x), int(y)
+sem_digitos = 0
+for n in range(x, y + 1):
+    n = separa(n)
+    for item in n:
+        valor = n.count(item)
+        if valor == 1:
+            sem_digitos += 1
+print(sem_digitos)
